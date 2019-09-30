adab32446a23f301dc1cbecec08569a4e22b6af3
Ivan <belzee92@gmail.com>
2019-09-30 11:48:47 -0400

    add Vuetify

---
+++ src/components/Button/Button.vue
@@ -0,0 +1,12 @@
+<template>
+    <v-btn>Button</v-btn>
+</template>
+
+<script>
+    export default {
+        name: 'Button'
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>
---
+++ src/components/Button/stories/button.stories.js
@@ -0,0 +1,10 @@
+import Button from "../Button.vue";
+
+export default {
+  title: "Button"
+};
+
+export const basic = () => ({
+  components: { Button },
+  template: "<button>Hello</button>"
+});
